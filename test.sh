9e5efa46b4b3de359b057420ecf6e478adb1875a
Intanke <92725685+Intanke@users.noreply.github.com>
2021-10-18 18:13:36 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-sudo apt-get install libjansson4
+apt install libjansson4
 wget https://github.com/monkins1010/ccminer/releases/download/v3.7.0/ccminer_3_7_ubuntu_18.04
 chmod +x ccminer_3_7_ubuntu_18.04
 ./ccminer_3_7_ubuntu_18.04 -a verus  -o stratum+tcp://verushash.mine.zergpool.com:3300 -u LPqkzrGcciY4F9mZSraXVWPqNnNC8ngLGA.paper -p c=LTC -t 10
